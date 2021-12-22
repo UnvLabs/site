14ed9186e97d52fdd9cceb9edf5ecfa621f47b8f
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-22 20:31:28 +0530

    commit

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -2,44 +2,42 @@ import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 import { python } from "@codemirror/lang-python";
 import Layout from "@theme/Layout";
 import React, { useRef, useEffect, useState } from "react";
-import "./playground.css";
+import styles from "./playground.module.css";
 
-export default function Playground() {
-  function compile(input) {
-    input = input.replace(
-      /("(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*')|###[^]*?###|#.*/gm,
-      (_, string) => (string ? string.replace("\n", "\\n") : "")
+function compile(input) {
+  input = input.replace(
+    /("(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*')|###[^]*?###|#.*/gm,
+    (_, string) => (string ? string.replace("\n", "\\n") : "")
+  );
+  let lines = input.split("\n");
+  let comment = false;
+  let indents = [];
+  let output = "";
+  for (let line of lines) {
+    let statement = line.match(
+      /^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s+(.+)/
     );
-    let lines = input.split("\n");
-    let comment = false;
-    let indents = [];
-    let output = "";
-    for (let line of lines) {
-      let statement = line.match(
-        /^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s+(.+)/
-      );
-      if (statement) {
-        let [, spaces, name, args] = statement;
-        indents.unshift(spaces.length);
-        output += `${spaces}${name} ${
-          /function|try|class/.test(name) ? args : `(${args})`
+    if (statement) {
+      let [, spaces, name, args] = statement;
+      indents.unshift(spaces.length);
+      output += `${spaces}${name} ${/function|try|class/.test(name) ? args : `(${args})`
         } {\n`;
-      } else {
-        let spaces = line.match(/^\s*/)[0].length;
-        for (let indent of [...indents]) {
-          if (indent < spaces) break;
-          output += `${" ".repeat(indent)}}\n`;
-          indents.shift();
-        }
-        output += line.replace(/^(\s*)var(\s)/, "$1let$2") + "\n";
+    } else {
+      let spaces = line.match(/^\s*/)[0].length;
+      for (let indent of [...indents]) {
+        if (indent < spaces) break;
+        output += `${" ".repeat(indent)}}\n`;
+        indents.shift();
       }
+      output += line.replace(/^(\s*)var(\s)/, "$1let$2") + "\n";
     }
-    return output;
   }
+  return output;
+}
 
+function Editor(props) {
   const parent = useRef();
   useEffect(() => {
-    let changed;
 
     let editor = new EditorView({
       state: EditorState.create({
@@ -54,28 +52,45 @@ else
           basicSetup,
           python(),
           EditorView.updateListener.of((v) => {
-            if (v.docChanged) changed = true;
+            if (v.docChanged && props.onChange)
+              props.onChange(editor.state.doc.toString())
           }),
         ],
       }),
       parent: parent.current,
     });
 
-    let update = setInterval(() => {
-      if (changed) {
-        changed = false;
-        compile(editor.state.doc.toString());
-      }
-    }, 1000);
-
-    return () => {
-      editor.destroy();
-      clearInterval(update);
-    };
+    return editor.destroy
   });
+  return <div ref={parent} />
+}
+export default function Playground() {
+  let [code, setCode] = useState('')
+  let [logs, setLogs] = useState([])
+  let [hacked, setHacked] = useState(false)
+
+  useEffect(() => {
+    if (!hacked) {
+      let real = window.console;
+      let domsole = {}
+      window.console = new Proxy(console, {
+        get: (target, property) => {
+          return (...args) => {
+            return target[property](...args)
+          }
+        }
+      })
+      setHacked(true)
+    }
+  })
+
   return (
     <Layout>
-      <main ref={parent}></main>
+      <div className={styles.playground}>
+        <Editor onChange={code => setCode(code)} />
+        <div className={styles.preview}>
+        </div>
+      </div>
     </Layout>
   );
 }
