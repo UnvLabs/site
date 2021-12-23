5303c776f0963dd82c7f731819126f8617046544
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-23 09:22:32 +0530

    Update playground.js

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,17 +1,8 @@
-import Layout from "@theme/Layout";
-import React, {useEffect, useState} from 'react';
-import clsx from 'clsx';
-import Highlight, {defaultProps, Language} from 'prism-react-renderer';
-import copy from 'copy-text-to-clipboard';
-import Translate, {translate} from '@docusaurus/Translate';
-import {
-  useThemeConfig,
-  parseLanguage,
-  parseLines,
-  ThemeClassNames,
-} from '@docusaurus/theme-common';
-import usePrismTheme from '@theme/hooks/usePrismTheme';
+import React, { Fragment, useEffect, useState } from 'react';
+import Editor from 'react-simple-code-editor';
+import Highlight, { Prism } from 'prism-react-renderer';
 import styles from "./playground.module.css";
+import usePrismTheme from '@theme/hooks/usePrismTheme';
 
 function compile(input) {
   input = input.replace(
@@ -44,122 +35,40 @@ function compile(input) {
   return output;
 }
 
-function Editor(props) {
-  const {prism} = useThemeConfig();
-
-  const [showCopied, setShowCopied] = useState(false);
-  const [mounted, setMounted] = useState(false);
-  const [code, setCode] = useState([[], '']);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const prismTheme = usePrismTheme();
-
-  const handleCopyCode = () => {
-    copy(code[1]);
-    setShowCopied(true);
-
-    setTimeout(() => setShowCopied(false), 2000);
-  };
-
-  return (
-    <Highlight
-      {...defaultProps}
-      key={String(mounted)}
-      theme={prismTheme}
-      code={code[1]}
-      language={'python' }>
-      {({ className, style, tokens, getLineProps, getTokenProps}) => (
-        <div
-          className={clsx(
-            styles.codeBlockContainer,
-            ThemeClassNames.common.codeBlock,
-          )}>
-          <div className={clsx(styles.codeBlockContent, 'python')}>
-            <pre
-              tabIndex={0}
-              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}
-              style={style}>
-              <code className={styles.codeBlockLines}>
-                {tokens.map((line, i) => {
-                  if (line.length === 1 && line[0].content === '\n') {
-                    line[0].content = '';
-                  }
-
-                  const lineProps = getLineProps({line, key: i});
-
-                  if (code[0].includes(i)) {
-                    lineProps.className += ' docusaurus-highlight-code-line';
-                  }
-
-                  return (
-                    <span key={i} {...lineProps}>
-                      {line.map((token, key) => (
-                        <span key={key} {...getTokenProps({token, key})} />
-                      ))}
-                      <br />
-                    </span>
-                  );
-                })}
-              </code>
-              <textarea className={styles.codeEditor} key={"static"} onChange={event => {
-                const {highlightLines, code} = parseLines(event.target.value, undefined, 'python');
-                setCode([highlightLines, code])
-              }}/>
-            </pre>
-            <button
-              type="button"
-              aria-label={translate({
-                id: 'theme.CodeBlock.copyButtonAriaLabel',
-                message: 'Copy code to clipboard',
-                description: 'The ARIA label for copy code blocks button',
-              })}
-              className={clsx(styles.copyButton, 'clean-btn')}
-              onClick={handleCopyCode}>
-              {showCopied ? (
-                <Translate
-                  id="theme.CodeBlock.copied"
-                  description="The copied button label on code blocks">
-                  Copied
-                </Translate>
-              ) : (
-                <Translate
-                  id="theme.CodeBlock.copy"
-                  description="The copy button label on code blocks">
-                  Copy
-                </Translate>
-              )}
-            </button>
-          </div>
-        </div>
-      )}
-    </Highlight>
-  );
-}
-
 export default function Playground() {
-  let [logs, setLogs] = useState([])
-  useEffect(() => {
-    window.print = (...args) => 
-        setLogs([
-          ...logs,
-          (<pre key={ logs.length }>
-            <code>{args.join}(' ')</code>
-          </pre>)
-        ])
-  })
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+
   return (
     <Layout>
       <div className={styles.playground}>
-        <Editor onChange={code => {
-          let fn = new Function(compile(code))
-          try {
-            fn()
-          } catch (e) {
-          }
-        }}/>
+        <Editor
+          value={code}
+          onValueChange={(code) => setCode(code)}
+          highlight={code => (
+    <Highlight
+      Prism={Prism}
+      code={code}
+      tabSize={4}
+      className={styles.codeEditor}
+      theme={usePrismTheme()}
+      language={'python'}
+    >
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <Fragment>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </Fragment>
+      )}
+    </Highlight>
+  );}
+        />
         <div className={styles.preview}>
           {logs}
         </div>
