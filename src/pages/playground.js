199d1f286d6554918d8013f1e0be3ba9fbbe4a9f
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-05 19:59:39 +0530

    Update playground.js

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -23,28 +23,47 @@ let declare_var = (ws, keyword, names) => {
     vars.map((v) => (~v.indexOf(",") ? "[" + v + "]" : v)).join("=");
   return code;
 };
+
 function compile(input) {
-  input = input.replace(
-    /("(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*')|###[^]*?###|#.*/gm,
-    (_, string) => (string ? string.replace(/\n/g, "\\n") : "")
-  );
-  let lines = input.split("\n");
-  let comment = false;
+  input =
+    input.replace(
+      /("(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*')|###[^]*?###|#.*/gm,
+      (_, string) => (string ? string.replace(/\n/g, "\\n") : "")
+    ) + "\n\n";
+  let lines = [];
+  let line = "";
+  let sqb = 0,
+    braces = 0,
+    parens = 0;
+  for (let char of input) {
+    if (char == "[") sqb++;
+    else if (char == "]") sqb--;
+    else if (char == "{") braces++;
+    else if (char == "}") braces--;
+    else if (char == "(") parens++;
+    else if (char == ")") parens--;
+    if (/[\n\r]/.test(char) && !sqb && !braces && !parens) {
+      lines.push(line);
+      line = "";
+    } else line += char;
+  }
   let indents = [];
   let output = "";
   for (let line of lines) {
     let statement = line.match(
-      /^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s+(.+)/
+      /^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s([^]+)/
     );
     if (statement) {
       let [, spaces, name, args] = statement;
       indents.unshift(spaces.length);
-      output +=
-        spaces +
-        name +
-        " " +
-        (/function|try|class/.test(name) ? args : "(" + args + ")") +
-        " {\n";
+      if (/for|catch/.test(name)) {
+        args = args.replace(
+          /^\s*([\w\s,]+)\sin\s([^]*)/,
+          (_, vars, val) => declare_var('', 'var', vars) + " of $iter(" + val + ")"
+        );
+      }
+      if (!/function|try|class/.test(name)) args = "(" + args + ")";
+      output += spaces + name + " " + args + " {\n";
     } else {
       let spaces = line.match(/^\s*/)[0].length;
       for (let indent of [...indents]) {
@@ -59,7 +78,7 @@ function compile(input) {
             (_, ws, names) => ws + "import {" + names + "} from"
           )
           .replace(
-            /^(\s*)((?:local\s|global\s)?)([\w\s,=]+)=(.*)/,
+            /^(\s*)((?:local\s|global\s)?)([\w\s,=]+)=([^]*)/,
             (_, ws, keyword, start, end) => {
               // choose the right keyword(let or var)
               let code = declare_var(
@@ -88,7 +107,7 @@ function compile(input) {
           ) + "\n";
     }
   }
-  return output;
+  return output.trim();
 }
 
 let sucrase =
