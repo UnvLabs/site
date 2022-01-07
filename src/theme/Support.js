9ed9965777ec8bc6616cec923e01e732a7b3f041
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-07 15:57:31 +0530

    Update Support.js

--- src/theme/Support.js
+++ src/theme/Support.js
@@ -11,6 +11,7 @@ let runtimes = {
 export default function Support(props) {
   return (
     <Admonition type="info" title="Supported Runtimes">
+      <p>Check is your flavour of Unv is supported.</p>
       <ul>
         {Object.entries(runtimes).map(([name, runtime], index) => (
           <li key={index}>
