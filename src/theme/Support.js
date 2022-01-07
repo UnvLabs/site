d2de62886e4cc662f3991788fdb0daf552a3ffcd
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-07 08:50:19 +0530

    Create Support.js

---
+++ src/theme/Support.js
@@ -0,0 +1,23 @@
+let runtimes = {
+  py: "Python",
+  js: "JavaScript",
+  rb: "Ruby",
+};
+
+export default function Support(props) {
+  return (
+    <div>
+      <b>Supported Runtimes</b>
+      <ul>
+        {Object.entries(runtimes).map(([name, runtime], index) => (
+          <li key={index}>
+            <label>
+              <input type="checkbox" checked={props[name]} readOnly={true} />
+              {runtime}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
