d3e09f06abc89c0c9c7b3e016094cd80f156a640
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-07 15:56:02 +0530

    Update Support.js

--- src/theme/Support.js
+++ src/theme/Support.js
@@ -1,4 +1,5 @@
 import Admonition from "@theme/Admonition";
+import Link from "@docusaurus/Link";
 import React from "react";
 
 let runtimes = {
@@ -13,7 +14,9 @@ export default function Support(props) {
       <ul>
         {Object.entries(runtimes).map(([name, runtime], index) => (
           <li key={index}>
-            {props[name] ? "✔️" : "❌"} {runtime}
+            <Link to={"/runtimes/" + name}>
+              {props[name] ? "✔️" : "❌"} {runtime}
+            </Link>
           </li>
         ))}
       </ul>
